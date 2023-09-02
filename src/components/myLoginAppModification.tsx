7ed9c569560385809cb12d31fe9a707faf94e3cd
ryuyafujisaki <ryuya.fujisaki@gmail.com>
2023-09-02 13:21:39 +0800

    started to create portfolio

---
+++ src/components/myLoginAppModification.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const myLoginAppModification = () => {
+    return (
+        <div>
+            作品１
+        </div>
+    )
+}
+
+export default myLoginAppModification
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,60 @@
 import Image from 'next/image'
 import { Inter } from 'next/font/google'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
+    <div
+      className=
+      "flex flex-col mx-8 my-10 lineCustom"
     >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
+      <Link id="link" href="https://my-login-app-modification.vercel.app/">
+        Vol13. FirebaseのAuthentificationとFirestoreを活用したTODOリスト
         <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+          src="/vol13.png"
+          alt=""
+          width={500}
+          height={300}
         />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      </Link>
+      <Link id="link" href="https://my-own-todo-app-10.vercel.app/">
+        Vol12. trello風TODOアプリ（TypeScript, Material UI）
+      </Link>
+      <Link id="link" href="https://master.d30n4n13k6fn7q.amplifyapp.com/">
+        Vol11. trello風TODOアプリ（JavaScript, Material UI）
+      </Link>
+      <Link id="link" href="https://master.d17vt2q5quk7jx.amplifyapp.com/">
+        Vol10. FIrebase/Firestore TODOアプリ（TypeScript, Material UI）
+      </Link>
+      <Link id="link" href="https://my-own-todo-app-7.vercel.app/">
+        Vol9. 各学習事項を意識しながらイチから作るTODOアプリ（TypeScript）
+      </Link>
+      <Link id="link" href="https://master.d36sqrx8bz2p6e.amplifyapp.com/">
+        Vol8. コンポーネント化を実施したTODOアプリ（TypeScript）
+      </Link>
+      <Link id="link" href="https://master.d2nbkk8ucu5sqh.amplifyapp.com/">
+        Vol7. コンポーネント化前のTODOアプリ（TypeScript）
+      </Link>
+      <Link id="link" href="https://easytodolist.vercel.app/">
+        Vol6. TypeScriptで初めてコンポーネント化に成功したTODOアプリ
+      </Link>
+      <Link id="link" href="https://my-own-todo-app-4.vercel.app/">
+        Vol5. 未完了/完了リストを表示するTODOリスト（TypeScript, Material UI）
+      </Link>
+      <Link id="link" href="https://todolistprototype.vercel.app/">
+        Vol4. コンポーネント化を実施したTODOアプリ（JavaScript）
+      </Link>
+      <Link id="link" href="https://twitter-clone-pi-mauve.vercel.app/">
+        Vol3. ReactとFirebaseを接続したTwitterクローン
+      </Link>
+      <Link id="link" href="https://react-tutorial-youtube.vercel.app/">
+        Vol2. React TODOリスト（JavaScript）
+      </Link>
+      <Link id="link" href="https://boisterous-churros-b7ffc7.netlify.app/">
+        Vol1. Vite, React TODOリスト（JavaScript）
+      </Link>
+    </div>
   )
 }
