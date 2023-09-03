7667af5e2adfbcaa4397da2058b2b5c72d404476
ryuyafujisaki <ryuya.fujisaki@gmail.com>
2023-09-03 09:32:45 +0800

    corrected style of app title and image size

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,160 +8,175 @@ export default function Home() {
   return (
     <div
       className=
-      "flex flex-col mx-8 my-30"
+      "flex flex-wrap mx-8 my-30"
     >
-      <div id='app'>
+      {/* flex-col→flex-wrap */}
+      <div id='app' className='w-1/2 p-4' >
+        {/* className='w-1/2 p-4'を追加 */}
         <Link id="link" href="https://my-login-app-modification.vercel.app/" target="_blank" >
-          Vol13. FirebaseのAuthentificationとFirestoreを活用したTODOリスト
+          <span style={{ fontSize: '24px', color: 'yellow' }}>Vol13.</span> FirebaseのAuthentificationとFirestoreを活用したTODOリスト
           <Image
             src="/vol13.png"
             alt=""
             width={500}
             height={300}
+            style={{ borderRadius: '10px' }}
           />
         </Link>
       </div>
 
-      <div id='app'>
+      <div id='app' className='w-1/2 p-4' >
         <Link id="link" href="https://my-own-todo-app-10.vercel.app/" target="_blank" >
-          Vol12. trello風TODOアプリ（TypeScript, Material UI）
+          <span style={{ fontSize: '24px', color: 'yellow' }}>Vol12.</span> trello風TODOアプリ（TypeScript, Material UI）
           <Image
             src="/vol12.png"
             alt=""
             width={500}
             height={300}
+            style={{ borderRadius: '10px' }}
           />
         </Link>
       </div>
 
-      <div id='app'>
+      <div id='app' className='w-1/2 p-4' >
         <Link id="link" href="https://master.d30n4n13k6fn7q.amplifyapp.com/" target="_blank">
-          Vol11. trello風TODOアプリ（JavaScript, Material UI）
+          <span style={{ fontSize: '24px', color: 'yellow' }}>Vol11.</span> trello風TODOアプリ（JavaScript, Material UI）
           <Image
             src="/vol11.png"
             alt=""
             width={500}
             height={300}
+            style={{ borderRadius: '10px' }}
           />
         </Link>
       </div>
 
-      <div id='app'>
+      <div id='app' className='w-1/2 p-4' >
         <Link id="link" href="https://master.d17vt2q5quk7jx.amplifyapp.com/" target="_blank">
-          Vol10. FIrebase/Firestore TODOアプリ（TypeScript, Material UI）
+          <span style={{ fontSize: '24px', color: 'yellow' }}>Vol10.</span> FIrebase/Firestore TODOアプリ（TypeScript, Material UI）
           <Image
             src="/vol10.png"
             alt=""
             width={500}
             height={300}
+            style={{ borderRadius: '10px' }}
           />
         </Link>
       </div>
 
-      <div id='app'>
+      <div id='app' className='w-1/2 p-4' >
         <Link id="link" href="https://my-own-todo-app-7.vercel.app/" target="_blank">
-          Vol9. 各学習事項を意識しながらイチから作るTODOアプリ（TypeScript）
+          <span style={{ fontSize: '24px', color: 'yellow' }}>Vol9.</span> 各学習事項を意識しながらイチから作るTODOアプリ（TypeScript）
           <Image
             src="/vol9.png"
             alt=""
             width={500}
             height={300}
+            style={{ borderRadius: '10px' }}
           />
         </Link>
       </div>
 
-      <div id='app'>
+      <div id='app' className='w-1/2 p-4' >
         <Link id="link" href="https://master.d36sqrx8bz2p6e.amplifyapp.com/" target="_blank">
-          Vol8. コンポーネント化を実施したTODOアプリ（TypeScript）
+          <span style={{ fontSize: '24px', color: 'yellow' }}>Vol8.</span> コンポーネント化を実施したTODOアプリ（TypeScript）
           <Image
             src="/vol8.png"
             alt=""
             width={500}
             height={300}
+            style={{ borderRadius: '10px' }}
           />
         </Link>
       </div>
 
-      <div id='app'>
+      <div id='app' className='w-1/2 p-4' >
         <Link id="link" href="https://master.d2nbkk8ucu5sqh.amplifyapp.com/" target="_blank">
-          Vol7. コンポーネント化前のTODOアプリ（TypeScript）
+          <span style={{ fontSize: '24px', color: 'yellow' }}>Vol7.</span> コンポーネント化前のTODOアプリ（TypeScript）
           <Image
             src="/vol7.png"
             alt=""
             width={500}
             height={300}
+            style={{ borderRadius: '10px' }}
           />
         </Link>
       </div>
 
-      <div id='app'>
+      <div id='app' className='w-1/2 p-4' >
         <Link id="link" href="https://easytodolist.vercel.app/" target="_blank">
-          Vol6. TypeScriptで初めてコンポーネント化に成功したTODOアプリ
+          <span style={{ fontSize: '24px', color: 'yellow' }}>Vol6.</span> TypeScriptで初めてコンポーネント化に成功したTODOアプリ
           <Image
             src="/vol6.png"
             alt=""
             width={500}
             height={300}
+            style={{ borderRadius: '10px' }}
           />
         </Link>
       </div>
 
-      <div id='app'>
+      <div id='app' className='w-1/2 p-4' >
         <Link id="link" href="https://my-own-todo-app-4.vercel.app/" target="_blank">
-          Vol5. 未完了/完了リストを表示するTODOリスト（TypeScript, Material UI）
+          <span style={{ fontSize: '24px', color: 'yellow' }}>Vol5.</span> 未完了/完了リストを表示するTODOリスト（TypeScript, Material UI）
           <Image
             src="/vol5.png"
             alt=""
             width={500}
             height={300}
+            style={{ borderRadius: '10px' }}
           />
         </Link>
       </div>
 
-      <div id='app'>
+      <div id='app' className='w-1/2 p-4' >
         <Link id="link" href="https://todolistprototype.vercel.app/" target="_blank">
-          Vol4. コンポーネント化を実施したTODOアプリ（JavaScript）
+          <span style={{ fontSize: '24px', color: 'yellow' }}>Vol4.</span> コンポーネント化を実施したTODOアプリ（JavaScript）
           <Image
             src="/vol4.png"
             alt=""
             width={500}
             height={300}
+            style={{ borderRadius: '10px' }}
           />
         </Link>
       </div>
 
-      <div id='app'>
+      <div id='app' className='w-1/2 p-4'>
         <Link id="link" href="https://twitter-clone-pi-mauve.vercel.app/" target="_blank">
-          Vol3. ReactとFirebaseを接続したTwitterクローン
+          <span style={{ fontSize: '24px', color: 'yellow' }}>Vol3.</span> ReactとFirebaseを接続したTwitterクローン
           <Image
             src="/vol3.png"
             alt=""
             width={500}
             height={300}
+            style={{ borderRadius: '10px' }}
           />
         </Link>
       </div>
 
-      <div id='app'>
+      <div id='app' className='w-1/2 p-4' >
         <Link id="link" href="https://react-tutorial-youtube.vercel.app/" target="_blank">
-          Vol2. React TODOリスト（JavaScript）
+          <span style={{ fontSize: '24px', color: 'yellow' }}>Vol2.</span> React TODOリスト（JavaScript）
           <Image
             src="/vol2.png"
             alt=""
             width={500}
             height={300}
+            style={{ borderRadius: '10px' }}
           />
         </Link>
       </div>
 
-      <div id='app'>
+      <div id='app' className='w-1/2 p-4' >
         <Link id="link" href="https://boisterous-churros-b7ffc7.netlify.app/" target="_blank">
-          Vol1. Vite, React TODOリスト（JavaScript）
+          <span style={{ fontSize: '24px', color: 'yellow' }}>Vol1.</span> Vite, React TODOリスト（JavaScript）
           <Image
             src="/vol1.png"
             alt=""
             width={500}
             height={300}
+            style={{ borderRadius: '10px' }}
           />
         </Link>
       </div>
